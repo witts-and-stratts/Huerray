946b25cb5c10838b8ee113f858939c3c7b1d04b0
Victory James Ugwudike <javik@live.com>
2025-11-21 13:58:22 +0100

    fixed Content for every objectives in brands

--- components/brands/BrandsContentObjective.tsx
+++ components/brands/BrandsContentObjective.tsx
@@ -15,36 +15,32 @@ export function BrandsContentObjective() {
     <section className='section-card section-card--pink mt-10 z-100'>
       <div className='section-card__container'>
         {/* Left - Image Collage */ }
-        <div className='section-card__image'>
-          <div className='brands-content-objective__images'>
-            <div className='brands-content-objective__image brands-content-objective__image--large'>
+        <div className='section-card__image flex flex-col gap-4'>
+          <Image
+            src='/images/brands/content-objective/ff36e4040fea432abfe66356bcfa58ee12e20e25.png'
+            alt='Creator content'
+            width={ 545 }
+            height={ 282 }
+            className='section-card__img'
+          />
+          <div className='grid grid-cols-6 gap-4 h-40'>
+            <div className='rounded-[64px] overflow-hidden col-span-4'>
               <Image
-                src='/images/brands/content-objective/ff36e4040fea432abfe66356bcfa58ee12e20e25.png'
-                alt='Creator content'
-                width={ 545 }
-                height={ 282 }
-                className='section-card__img'
+                src='/images/brands/content-objective/orange-products.png'
+                alt='Beauty products'
+                width={ 262 }
+                height={ 172 }
+                className='object-cover w-full h-full'
               />
             </div>
-            <div className='grid grid-cols-6 gap-4 h-40'>
-              <div className='rounded-[64px] overflow-hidden col-span-4'>
-                <Image
-                  src='/images/brands/content-objective/orange-products.png'
-                  alt='Beauty products'
-                  width={ 262 }
-                  height={ 172 }
-                  className='object-cover w-full h-full'
-                />
-              </div>
-              <div className='rounded-[64px] overflow-hidden col-span-2'>
-                <Image
-                  src='/images/brands/content-objective/woman-yellow.png'
-                  alt='Creator'
-                  width={ 262 }
-                  height={ 172 }
-                  className='object-cover w-full h-full'
-                />
-              </div>
+            <div className='rounded-[64px] overflow-hidden col-span-2'>
+              <Image
+                src='/images/brands/content-objective/woman-yellow.png'
+                alt='Creator'
+                width={ 262 }
+                height={ 172 }
+                className='object-cover w-full h-full'
+              />
             </div>
           </div>
         </div>
